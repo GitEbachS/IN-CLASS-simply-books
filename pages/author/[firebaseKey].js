968cfc13f6ab60652ca290ae523538ebd4c7166b
Shari Ebach <ebachshari@yahoo.com>
2023-10-09 21:27:24 -0500

    author details

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,4 +1,26 @@
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { viewAuthorDetails } from '../../api/mergedData';
+import BookCard from '../../components/BookCard';
 
-// const router = useRouter();
-// const { firebaseKey } = router.query;
+export default function ViewAuthor() {
+  const [authorDetails, setAuthorDetails] = useState({});
+  const router = useRouter();
+
+  const { firebaseKey } = router.query;
+
+  const getADetails = () => {
+    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+  };
+
+  useEffect(() => {
+    getADetails();
+  }, []);
+
+  return (
+    <div>{authorDetails.books?.map((book) => (
+      <BookCard key={book.firebaseKey} bookObj={book} onUpdate={getADetails} />
+    ))}
+    </div>
+  );
+}
